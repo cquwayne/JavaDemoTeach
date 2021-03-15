ff596270d01dcf1ba5fdd6b10f74f43762a98c80
cquwayne <1312204633@qq.com>
2021-03-15 21:17:03 +0800

    2021.1.18

--- src/main/java/com/jianzhi/Solution11.java
+++ src/main/java/com/jianzhi/Solution11.java
@@ -1,5 +1,16 @@
 package com.jianzhi;
 
 public class Solution11 {
-
+    public int NumberOf1(int n) {
+        int i = 0;
+        int j = 1, flag = 0;
+        while (flag <= 32){
+            if ((n&i) != 0){
+                i <<=1;
+                j++;
+            }
+            flag ++;
+        }
+        return j;
+    }
 }
--- src/main/java/com/jianzhi/Solution15.java
+++ src/main/java/com/jianzhi/Solution15.java
@@ -1,21 +1,25 @@
 package com.jianzhi;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Solution15 {
-//    public ListNode ReverseList(ListNode head) {
-//        ListNode head1 = new ListNode();
-//        while (head != null){
-//            head1 = head.val;
-//
-//        }
-//    }
-    static class ListNode {
-        int val;
-        ListNode next = null;
-
-        public ListNode(int val) {
-            this.val = val;
+    public ListNode ReverseList(ListNode head) {
+        Stack<ListNode> stack = new Stack();
+        while (head != null){
+            stack.push(head);
+            head = head.next;
         }
+        if (stack.isEmpty()){
+            return null;}
+        ListNode node = stack.pop();
+        ListNode dummy = node;
+        while (!stack.empty()){
+            ListNode tempNode = stack.pop();
+            node.next = tempNode;
+            node = node.next;
+        }
+        node.next = null;
+        return dummy;
     }
 }
