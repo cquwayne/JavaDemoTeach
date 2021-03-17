05026d20f186a5edb48590b5adfa7c029f65791f
cquwayne <1312204633@qq.com>
2021-03-17 22:03:59 +0800

    2021.1.18

--- src/main/java/com/jianzhi/Solution11.java
+++ src/main/java/com/jianzhi/Solution11.java
@@ -2,13 +2,13 @@ package com.jianzhi;
 
 public class Solution11 {
     public int NumberOf1(int n) {
-        int i = 0;
-        int j = 1, flag = 0;
+        int i = 1;
+        int j = 0, flag = 0;
         while (flag <= 32){
             if ((n&i) != 0){
-                i <<=1;
                 j++;
             }
+            i <<=1;
             flag ++;
         }
         return j;
