a13a7dddfc7d68fd5c46c01cd3603d44ae47a5c3
cquwayne <1312204633@qq.com>
2021-04-01 19:20:29 +0800

    2021.1.18

--- src/main/java/com/jianzhi/Solution38.java
+++ src/main/java/com/jianzhi/Solution38.java
@@ -1,5 +1,17 @@
 package com.jianzhi;
 
+
+ class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+
 public class Solution38 {
     public int TreeDepth(TreeNode root) {
         if (root == null){
---
+++ src/main/java/com/jianzhi/Solution54.java
@@ -0,0 +1,16 @@
+package com.jianzhi;
+
+public class Solution54 {
+    //Insert one char from stringstream
+    public void Insert(char ch)
+    {
+
+    }
+    //return the first appearence once char in current stringstream
+    public char FirstAppearingOnce()
+    {
+
+        return 0;
+    }
+
+}
